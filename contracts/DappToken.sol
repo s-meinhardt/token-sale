820f290c84b221b7678a686addf0a101cca0b1f9
Sven Meinhardt <sven.meinhardt@posteo.de>
2020-11-21 09:01:09 +0000

    1. Smoke Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.2 <0.8.0;
+
+contract DappToken {
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000;
+    }
+}
