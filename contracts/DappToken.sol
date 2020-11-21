e4438d9be386d8d9bc1b8a9a5994e5bf5d16df3d
Sven Meinhardt <sven.meinhardt@posteo.de>
2020-11-21 14:53:39 +0000

    2. Transfer tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,11 +1,29 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.4.2 <0.8.0;
+pragma solidity ^0.7.0;
 
 contract DappToken {
     uint256 public totalSupply;
+    string public name = "DApp Token";  // optional
+    string public symbol =  "DAPP";     // optional
+    string public standard = "DApp Token v1.0";  // not part of the ERC20 standard
 
-    constructor() public {
-        totalSupply = 1000000;
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) {
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
     }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+      require(balanceOf[msg.sender]>= _value, "Not enough tokens to transfer");
+      balanceOf[msg.sender] -= _value;
+      balanceOf[_to] += _value;
+      emit Transfer(msg.sender, _to, _value);
+      return true;
+    }
+
+    
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,18 +1,88 @@
-let DappToken = artifacts.require('./DappToken.sol')
+let DappToken = artifacts.require('DappToken')
 
 contract('DappToken', (accounts) => {
-  it('sets the total supply upon deployment', () => {
-    return DappToken.deployed()
-      .then((instance) => {
-        tokenInstance = instance
-        return tokenInstance.totalSupply()
-      })
-      .then((totalSupply) => {
-        assert.equal(
-          totalSupply.toNumber(),
-          1000000,
-          'sets the total supply to 1000000'
-        )
-      })
+  it('initializes the contract with the correct values', async () => {
+    let instance = await DappToken.deployed()
+    let name = await instance.name()
+    assert.equal(name, 'DApp Token', 'has the correct name')
+    let symbol = await instance.symbol()
+    assert.equal(symbol, 'DAPP', 'has the correct symbol')
+    let standard = await instance.standard()
+    assert.equal(standard, 'DApp Token v1.0', 'has the correct standard')
+  })
+
+  it('allocates the initial supply upon deployment', async () => {
+    let instance = await DappToken.deployed()
+
+    let totalSupply = await instance.totalSupply()
+    assert.equal(
+      totalSupply.toNumber(),
+      1000000,
+      'sets the total supply to 1,000,000'
+    )
+
+    let adminBalance = await instance.balanceOf(accounts[0])
+    assert.equal(
+      adminBalance.toNumber(),
+      1000000,
+      'it allocates the initial supply to the admin account'
+    )
+  })
+
+  it('transfers token ownership', async () => {
+    let instance = await DappToken.deployed()
+
+    try {
+      await instance.transfer.call(accounts[1], 999999999999999)
+    } catch (error) {
+      assert(
+        error.message.indexOf('revert') >= 0,
+        'error message must contain revert'
+      )
+    }
+
+    let success = await instance.transfer.call(accounts[1], 250000, {
+      from: accounts[0],
+    })
+    assert.equal(success, true, 'it returns true')
+
+    let receipt = await instance.transfer(accounts[1], 250000, {
+      from: accounts[0],
+    })
+    assert.equal(receipt.logs.length, 1, 'triggers one event')
+    assert.equal(
+      receipt.logs[0].event,
+      'Transfer',
+      'should be the "Transfer" event'
+    )
+    assert.equal(
+      receipt.logs[0].args._from,
+      accounts[0],
+      'logs the account the tokens are transferred from'
+    )
+    assert.equal(
+      receipt.logs[0].args._to,
+      accounts[1],
+      'logs the account the tokens are transferred to'
+    )
+    assert.equal(
+      receipt.logs[0].args._value,
+      250000,
+      'logs the transfer amount'
+    )
+
+    let balanceReceiver = await instance.balanceOf(accounts[1])
+    assert.equal(
+      balanceReceiver.toNumber(),
+      250000,
+      'adds the amount to the receiving account'
+    )
+
+    let balanceSender = await instance.balanceOf(accounts[0])
+    assert.equal(
+      balanceSender.toNumber(),
+      750000,
+      'deducts the amount to the sending account'
+    )
   })
 })
